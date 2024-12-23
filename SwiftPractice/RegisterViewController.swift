973a08204ddd97261dc63693f6f40a9f8497699d
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-12-23 22:56:32 +0300

    textfiled bug fix

--- SwiftPractice/RegisterViewController.swift
+++ SwiftPractice/RegisterViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
     
     private let nameSurnameTextField: UITextField = {
        var textField = UITextField()
         textField.placeholder = "Adınızı ve soyadınızı giriniz"
         textField.borderStyle = .roundedRect
-        textField.font = .systemFont(ofSize: 14)
+        textField.keyboardType = .alphabet
+        textField.autocapitalizationType = .words
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
@@ -23,7 +24,8 @@ class RegisterViewController: UIViewController {
         textField.placeholder = "Email adresinizi giriniz"
         textField.borderStyle = .roundedRect
         textField.keyboardType = .emailAddress
-        textField.font = .systemFont(ofSize: 14)
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
@@ -33,7 +35,7 @@ class RegisterViewController: UIViewController {
         textField.placeholder = "Şifrenizi giriniz"
         textField.borderStyle = .roundedRect
         textField.isSecureTextEntry = true
-        textField.font = .systemFont(ofSize: 14)
+        textField.autocapitalizationType = .none
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
@@ -43,7 +45,7 @@ class RegisterViewController: UIViewController {
         textField.placeholder = "Şifrenizi tekrar giriniz"
         textField.borderStyle = .roundedRect
         textField.isSecureTextEntry = true
-        textField.font = .systemFont(ofSize: 14)
+        textField.autocapitalizationType = .none
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
@@ -72,15 +74,20 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
+        nameSurnameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordAgainTextField.delegate = self
     }
     
     private func setupUI() {
+        title = "Kayıt Ol"
         view.backgroundColor = .appBackGround
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Geri", style: .plain, target: self, action: #selector(dismissButtonPressed))
-        navigationController?.navigationBar.tintColor = .appGreen
+        navigationItem.leftBarButtonItem?.tintColor = .appGreen
         view.addSubview(nameSurnameTextField)
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
@@ -92,34 +99,34 @@ class RegisterViewController: UIViewController {
         view.addGestureRecognizer(tap)
         
         NSLayoutConstraint.activate([
-            nameSurnameTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -130),
+            nameSurnameTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200),
             nameSurnameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
             nameSurnameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
             nameSurnameTextField.heightAnchor.constraint(equalToConstant: 44),
             
             emailTextField.topAnchor.constraint(equalTo: nameSurnameTextField.bottomAnchor, constant: 10),
-            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
-            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            emailTextField.leadingAnchor.constraint(equalTo: nameSurnameTextField.leadingAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: nameSurnameTextField.trailingAnchor),
             emailTextField.heightAnchor.constraint(equalToConstant: 44),
             
             passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 10),
-            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
-            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            passwordTextField.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
             passwordTextField.heightAnchor.constraint(equalToConstant: 44),
             
             passwordAgainTextField.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 10),
-            passwordAgainTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
-            passwordAgainTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            passwordAgainTextField.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+            passwordAgainTextField.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
             passwordAgainTextField.heightAnchor.constraint(equalToConstant: 44),
             
             loginButton.topAnchor.constraint(equalTo: passwordAgainTextField.bottomAnchor, constant: 10),
-            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
-            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            loginButton.leadingAnchor.constraint(equalTo: passwordAgainTextField.leadingAnchor),
+            loginButton.trailingAnchor.constraint(equalTo: passwordAgainTextField.trailingAnchor),
             loginButton.heightAnchor.constraint(equalToConstant: 44),
             
             dismissButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10),
-            dismissButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
-            dismissButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            dismissButton.leadingAnchor.constraint(equalTo: loginButton.leadingAnchor),
+            dismissButton.trailingAnchor.constraint(equalTo: loginButton.trailingAnchor),
             dismissButton.heightAnchor.constraint(equalToConstant: 44)
             ])
         
@@ -132,11 +139,48 @@ class RegisterViewController: UIViewController {
     }
     
     @objc func loginButtonPressed() {
+        guard let nameSurname = nameSurnameTextField.text , nameSurname != "" else {
+            showAlertController(title: "Hata", message: "Ad Soyad alanı boş bırakılamaz")
+            return
+        }
         
+        guard let email = emailTextField.text , email != "" else {
+            showAlertController(title: "Hata", message: "Lütfen geçerli bir mail adresi giriniz")
+            return
+        }
+        
+        guard let password = passwordTextField.text , password != "" else {
+            showAlertController(title: "Hata", message: "Lütfen şifre giriniz")
+            return
+        }
+        guard let passwordAgain = passwordAgainTextField.text , passwordAgain != "" else {
+            showAlertController(title: "Hata", message: "Lütfen şifrenizi tekrar giriniz")
+            return
+        }
+        
+        if password.lowercased() != passwordAgain.lowercased() {
+           showAlertController(title: "Hata", message: "Şifreler Aynı Değil")
+           return
+        }
+        
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    private func showAlertController(title : String , message : String, completion : (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Tamam", style: .default) { _ in
+            completion?()
+        })
+        present(alert, animated: true)
     }
 
 }
